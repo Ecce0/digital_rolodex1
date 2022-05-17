5abad41dfa7e5f8990175deb4cba89ed1cda9db5
E. Collier <ecollier871@outlook.com>
2022-05-17 14:39:58 -0400

    validation added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
+const { check, validationResult } = require('express-validator/check')
 
 
 //@route  POST api/users
 //@desc   Register a user
 //@access Public
-router.post('/', (req, res) => {
-    res.send('Registers a user')
+router.post('/', [
+    check('name', 'Please add nane').not().isEmpty(),
+    check('email', 'Plase include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({ error: errors.array() })
+    }
+
+    res.send('passed')
 })
 
 
