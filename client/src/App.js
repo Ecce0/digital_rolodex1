1ef38303b184c089a8960f3c24c70d155a1af210
E. Collier <ecollier871@outlook.com>
2022-05-18 17:23:04 -0400

    home, navbar, and about pages added to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
+import React from 'react'
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+     <div className="App">
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+        </Routes>
+      </div>
+     </div>
+    </Router>
+    
   );
 }
 
