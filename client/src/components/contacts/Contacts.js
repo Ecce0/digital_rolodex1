eaf14383d9685a8a791dc45660627402aa1287d9
E. Collier <ecollier871@outlook.com>
2022-05-19 19:46:49 -0400

    added css effects

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,28 +1,31 @@
 import React, { useContext } from 'react'
 import ContactContext from '../context/contact/ContactContext'
 import ContactItem from './ContactItem'
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const Contacts = () => {
-    const contactContext = useContext(ContactContext)
+	const contactContext = useContext(ContactContext)
 	const { contacts, filtered } = contactContext
 
-	if(contacts.length === 0 ){
+	if (contacts.length === 0) {
 		return <h4>Please add a contact</h4>
 	}
 
 	return (
 		<>
-			{filtered !== null ? filtered.map(contact => 
-			 (<ContactItem key={contact.id} contact={contact} />)) 
-			
-			: 
-			 
-			contacts.map((contact) => (
-				<ContactItem key={contact.id} contact={contact} />
-			))}
-			
-		    
+			<TransitionGroup>
+				{filtered !== null
+					? filtered.map((contact) => (
+							<CSSTransition key={contact.id} timeout={500} className='item'>
+								<ContactItem  contact={contact} />
+							</CSSTransition>
+					  ))
+					: contacts.map((contact) => (
+							<CSSTransition key={contact.id} timeout={500} className='item'>
+								<ContactItem  contact={contact} />
+							</CSSTransition>
+					  ))}
+			</TransitionGroup>
 		</>
 	)
 }
