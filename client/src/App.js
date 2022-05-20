a39cf7bc060d4dc966f5c832960f42c7b9e158b6
E. Collier <ecollier871@outlook.com>
2022-05-20 11:04:45 -0400

    alert error fixed, state updated and input attributes added

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,39 @@
 import React from 'react'
-import './App.css';
+import './App.css'
 import Navbar from './components/layout/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactState from './components/context/contact/ContactState'
 import AuthState from './components/context/auth/AuthState'
+import AlertState from './components/context/alert/AlertState'
 import Register from './components/auth/Register'
 import Login from './components/auth/Login'
+import Alerts from './components/layout/Alerts'
 
 const App = () => {
-  return (
-    <AuthState>
-    <ContactState>
-    <Router>
-     <div className="App">
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/register' element={<Register />} />
-          <Route exact path='/login' element={<Login />} />
-        </Routes>
-      </div>
-     </div>
-    </Router>
-    </ContactState>  
-    </AuthState>  
-  )
+	return (
+		<AuthState>
+			<ContactState>
+        <AlertState>
+        <Router>
+			<div className='App'>
+				<Navbar />
+				<div className='container'>
+                  <Alerts />
+				<Routes>
+					<Route exact path='/' element={<Home />} />
+					<Route exact path='/about' element={<About />} />
+					<Route exact path='/register' element={<Register />} />
+					<Route exact path='/login' element={<Login />} />
+				</Routes>
+					</div>
+				</div>
+				</Router>
+        </AlertState>
+			</ContactState>      
+		</AuthState>
+	)
 }
 
-export default App;
+export default App
