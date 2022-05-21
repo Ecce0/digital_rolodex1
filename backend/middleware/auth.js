11f373e034873111b7546f3cf29468bf16f80f4e
E. Collier <ecollier871@outlook.com>
2022-05-21 18:26:22 -0400

    loadUser the issue. Not localStorage, therefore no token being set, but why?

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,11 +5,12 @@ module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
   
-  // Check if not token
+  // Check if no token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });    
+    
   }
-
+ 
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
     req.user = decoded.user;
