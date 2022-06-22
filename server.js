3db7c5d22ee0bfbfa81c1691c968df20bfe31ab0
E. Collier <ecollier871@outlook.com>
2022-06-22 14:13:09 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/db')
+const path = require('path')
 
 
 //Connect database
@@ -14,6 +15,13 @@ app.use(express.json({ extended: false }))
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html' )))
+}
 
 const PORT = process.env.PORT || 4444
 app.listen(PORT, () => console.log(`app running on ${PORT}`))
